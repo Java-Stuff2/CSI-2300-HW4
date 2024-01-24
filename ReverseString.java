100545bc24f37be931d3d2e0fffad0797f16039f
Java-Stuff2 <156843611+Java-Stuff2@users.noreply.github.com>
2024-01-24 15:23:18 -0500

    Add files via upload

--- ReverseString.java
+++ ReverseString.java
@@ -1,4 +1,6 @@
 
+import java.util.Scanner;
+
 public class ReverseString {
     public static void main(String[] args) throws Exception {
 
@@ -12,6 +14,15 @@ public class ReverseString {
             nstr = ch + nstr; // adds each character in front of the existing string
         }
         System.out.println(nstr);
-
+        Scanner scnnr = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String stri = scnnr.nextLine();
+        scnnr.close();
+        char[] arr = stri.toCharArray();
+        String rev = "";
+        for (int i = stri.length() - 1; i >= 0; i--) {
+            rev = rev + stri.charAt(i);
+        }
+        System.out.println("Hello, " + rev + " lovely name!");
     }
 }
